fdeefb3fa6c60e8b2edd1a2ce6651a4e5d29e9c1
Alen <alechkr@gmail.com>
2020-10-31 16:05:13 +0100

    changed the gsap animation slightly

--- script/script.js
+++ script/script.js
@@ -9,9 +9,9 @@ tl.from('.third', {duration: 1, x: 100, opacity: 0});
 tl.from('.fourth', {duration: 1, x: 100, opacity: 0}); */
 
 gsap.from('.first', {scrollTrigger: {trigger: '.first', start: 'top center'}, duration: 1, x: 100, opacity: 0});
-gsap.from('.second', {scrollTrigger: {trigger: '.second', start: 'top center'}, duration: 1, x: 100, opacity: 0});
+gsap.from('.second', {scrollTrigger: {trigger: '.second', start: 'top center'}, duration: 1, x: -100, opacity: 0});
 gsap.from('.third', {scrollTrigger: {trigger: '.third', start: 'top center'}, duration: 1, x: 100, opacity: 0});
-gsap.from('.fourth', {scrollTrigger: {trigger: '.fourth', start: 'top center'}, duration: 1, x: 100, opacity: 0});
+gsap.from('.fourth', {scrollTrigger: {trigger: '.fourth', start: 'top center'}, duration: 1, x: -100, opacity: 0});
 
 
 gsap.from("#primary-2", {
