86a5edb3b9030740c9dda4541d2a16e1e8148f52
Alen <alechkr@gmail.com>
2020-10-31 14:42:50 +0100

    added scrollTrigger

--- script/script.js
+++ script/script.js
@@ -1,5 +1,14 @@
 /* gsap.to("#primary-1", {duration: 2, x: 300}); */
-gsap.from('#carousel', {opacity: 0, duration: 1, y:-400});
+
+/* gsap.from('#carousel', {opacity: 0, duration: 1, y:-400});
 gsap.from('#icons', {opacity: 0, duration: 1, x: 500});
+ */
+
+gsap.registerPlugin(ScrollTrigger);
 
 
+
+
+
+gsap.from('.first', {scrollTrigger: 'first', duration: 1, opacity: 0, x: 300});
+gsap.from('.second', {scrollTrigger: 'second', duration: 1, opacity: 0, x: 300});
