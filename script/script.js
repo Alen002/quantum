2a2b7922fa83de8306dd65daadfc06d7ab1f4b86
Alen <alechkr@gmail.com>
2020-10-31 10:22:51 +0100

    added sass

--- script/script.js
+++ script/script.js
@@ -1,3 +1,5 @@
 /* gsap.to("#primary-1", {duration: 2, x: 300}); */
-gsap.from('#carousel', {opacity: 0, duration: 2, x: -500});
+gsap.from('#carousel', {opacity: 0, duration: 1, y:-400});
+gsap.from('#icons', {opacity: 0, duration: 1, x: 500});
+
 
